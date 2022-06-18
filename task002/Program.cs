f0ce9f6f99d34403926dc74d8a5587d7d82956cd
Andrey Vedischev <vedischev76@mail.ru>
2022-06-18 18:56:36 +0500

    add task 002

---
+++ task002/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите четверть: ");
+int A =int.Parse(ReadLine());
+
+string x = "", y = "";
+
+if(A == 1)
+{
+    x = "x+";
+    y = "y+";
+}
+if(A == 2)
+{
+    x = "x-";
+    y = "y+";
+}
+if(A == 3)
+{
+    x = "x-";
+    y = "y-";
+}
+if(A == 4)
+{
+    x = "x+";
+    y = "y-";
+}
+WriteLine($"{x}, {y}");
