9c1488254c4f7ecbe627cf6238993faf0d3c868e
Andrey Vedischev <vedischev76@mail.ru>
2022-06-18 19:48:56 +0500

    add task 004

---
+++ task004/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+int N = Convert.ToInt32(ReadLine());
+WriteLine();
+
+for(int i = 1; i <= N; i++)
+{
+    WriteLine(Math.Pow(i, 2));
+}
+WriteLine();
