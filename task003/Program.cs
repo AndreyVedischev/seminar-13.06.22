02c67fc94b9030ca6580f20dfd17e5aed9f27e28
Andrey Vedischev <vedischev76@mail.ru>
2022-06-18 19:11:55 +0500

    add task 003

---
+++ task003/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите четверть: ");
+int q =int.Parse(ReadLine());
+
+switch(q)
+{
+    case 1:
+    {
+        Writeline("x > 0, y > 0");
+        break;
+    }
+    case 2:
+    {
+        Writeline("x < 0, y > 0");
+        break;
+    }
+    case 3:
+    {
+        Writeline("x < 0, y < 0");
+        break;
+    }
+    case 4:
+    {
+        Writeline("x > 0, y < 0");
+        break;
+    }
+}
