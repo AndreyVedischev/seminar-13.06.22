9013beec43d3b79d20a63e0342d5154d2a34f49a
Andrey Vedischev <vedischev76@mail.ru>
2022-06-18 20:04:30 +0500

    add task 005

---
+++ task005/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите N: ");
+int N = Convert.ToInt32(ReadLine());
+WriteLine();
+for(int i = 2; i <= N; i+=2)
+{
+    Write($"{i}, ");
+}
